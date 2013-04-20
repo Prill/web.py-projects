3aee6be0ab1a264dcd9ac40df0aee78c1edcab2d
Max W Schwarz <wren@cat.pdx.edu>
2013-04-19 22:55:06 -0700

    Add model for viewing individual row

--- todo2/model.py
+++ todo2/model.py
@@ -1,8 +1,15 @@
 import web
 
-db = web.database (dbn="sqlite", name="data/main.sqlite3")
+db = web.database (dbn="sqlite", db="data/main.sqlite3")
 
 def get_all_todos():
     return db.select('todo', order='id')
 
-
+def get_todo(todo_id):
+    rows = db.select('todo', where='id=$todo_id', vars=locals())
+    rowslist = list(rows)
+    print len(rowslist), rowslist
+    if len(rowslist) == 0:
+        return None
+    else:
+        return rowslist[0]
--- todo2/todo.py
+++ todo2/todo.py
@@ -5,12 +5,19 @@ render = web.template.render("templates", base="base")
 
 urls = (
     '/', 'Index',
+    '/view/(\d+)/?', "View",
 )
 
 class Index:
     def GET(self):
         todos = model.get_all_todos()
         return render.index(todos)
+
+class View:
+    def GET(self,todo_id):
+        todo = model.get_todo(todo_id)
+        return render.view(todo)
+
 
 if __name__ == '__main__':
     app = web.application(urls, globals())
